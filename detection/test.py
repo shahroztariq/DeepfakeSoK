317b3639145986d8abba743ada139054e4b6ab43
Le Minh Binh <binhleminh0209@gmail.com>
2024-05-23 18:29:12 +0900

    Update test.py

--- detection/test.py
+++ detection/test.py
@@ -222,11 +222,12 @@ def main(args):
         model.cuda()
         transform = transforms.Compose([
                     transforms.Resize([256, 256]),
-                    transforms.RandomCrop([224, 224]),
+                    transforms.CenterCrop([224, 224]),
                     transforms.ToTensor(),
                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                 ])
-                    
+        softmax = False
+        model.eval()
 
     elif args.model_name == 'effb4att':
         net_model = 'EfficientNetAutoAttB4ST'
