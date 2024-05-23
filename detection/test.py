8e7a9f26f9576943196ea9800aa47b8e1f2296a9
Le Minh Binh <binhleminh0209@gmail.com>
2024-05-23 18:26:58 +0900

    Update test.py

--- detection/test.py
+++ detection/test.py
@@ -222,8 +222,9 @@ def main(args):
         model.cuda()
         transform = transforms.Compose([
                     transforms.Resize([256, 256]),
-                    transforms.RandomCrop([256, 256]),
+                    transforms.RandomCrop([224, 224]),
                     transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                 ])
                     
 
